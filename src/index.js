c480ff7dacce54b216f25e347eb3ab136f8f6da3
Lesley Labady <labadylesley@gmail.com>
2022-07-26 21:34:21 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const taskForm= document.getElementById("create-task-form")
+
+  const tasklist =document.getElementById("tasks")
+
+  taskForm.addEventListener('submit',e => {
+  e.preventDefault()
+  const newTask = document.getElementById("new-task-description").value
+  const taskItem = document.createElement("p")
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  taskItem.innerText = `${newTask} `
+  taskItem.appendChild(btn)
+  tasklist.appendChild(taskItem)
+  taskForm.reset()
+})
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+})
